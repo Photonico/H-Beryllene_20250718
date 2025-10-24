102ac2ff349666cccd5a796bdb51db8fa20a820c
Photonico <LukeNiu@outlook.com>
2025-10-24 14:58:00 +1100

    save

--- 2.0_geometry_optimization/g1-Beryllene_monolayer/vasp_cluster.sh
+++ 2.0_geometry_optimization/g1-Beryllene_monolayer/vasp_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/csh
-#PBS -N convergence
+#PBS -N convergence_g1
 #PBS -q cmt
 #PBS -j oe
 #PBS -l select=1:ncpus=42:mpiprocs=42:mem=200GB
--- 2.1_self_consistent/g1-Beryllene_monolayer/vasp_cluster.sh
+++ 2.1_self_consistent/g1-Beryllene_monolayer/vasp_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/csh
-#PBS -N SCF_g-Beryllene
+#PBS -N SCF_g1
 #PBS -q cmt
 #PBS -j oe
 #PBS -l select=1:ncpus=42:mpiprocs=42:mem=200GB
